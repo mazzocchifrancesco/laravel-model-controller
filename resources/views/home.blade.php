449c417684bc4f1b98c59d4225890d1606030581
FrancescoMazzocchi <123@email.it>
2024-01-22 15:50:45 +0100

    add scss style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>FILM</h1>
+<h1 class="text-center my-5">FILM</h1>
+
+<div class="container">
+
+    <div class="row">
+        
+        @foreach ($movie as $film)
+        <div class="col-3 my-5">
+                    <!-- inizio card  -->
+                    <div class="cardContainer p-3 rounded">
+                        <h5 class="card-title pb-5">{{$film->title}}</h5>
+                        <p><strong>Titolo originale: </strong>{{$film->original_title}}</p>
+                        <p><strong>Nazionalità: </strong>{{$film->nationality}}</p>
+                        <p><strong>Data: </strong>{{$film->date}}</p>
+                        <p><strong>Voto: </strong>{{$film->vote}}/10</p>
 
 
-<p>This is my paragraph content.</p>
-
-@foreach ($movie as $film)
-    <p>{{$film['title']}}</p>
-@endforeach
+                    </div>
+                </div>
+                @endforeach
+    </div>
+</div>
 
 
 @endsection
